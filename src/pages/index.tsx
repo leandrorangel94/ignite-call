383b7c082a6f56a49c5656beab8cabb0e2d72a78
Leandro Rangel <rangel94@outlook.com>
2023-06-26 10:59:12 -0300

    Setup & estrutura - Instalação do Design System

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import {Heading, Text} from '@ignite-ui/react';
+
 export default function Home() {
   return (
     <>
-     <h1>Home</h1>
+     <Heading as='h1'>Home</Heading>
+     <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut asperiores, debitis laborum ipsum dolorum beatae exercitationem minus aliquam laboriosam, doloribus, adipisci autem sint! Fuga officia ducimus commodi laborum autem. Molestias?</Text>
     </>
   )
 }

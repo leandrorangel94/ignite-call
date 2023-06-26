89aa941aafd348c9142e808e268f0c9c232aad53
Leandro Rangel <rangel94@outlook.com>
2023-06-26 11:56:04 -0300

    Cadastro de usuário - Estrutura visual da home

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,15 +1,1 @@
-import { Heading, Text } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <>
-      <Heading as="h1">Home</Heading>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut asperiores,
-        debitis laborum ipsum dolorum beatae exercitationem minus aliquam
-        laboriosam, doloribus, adipisci autem sint! Fuga officia ducimus commodi
-        laborum autem. Molestias?
-      </Text>
-    </>
-  )
-}
+export { default } from './home'

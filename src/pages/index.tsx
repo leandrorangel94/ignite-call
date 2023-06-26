dd9b809eef98738721fd493c723ddda12f6d79a5
Leandro Rangel <rangel94@outlook.com>
2023-06-26 11:04:21 -0300

    Setup & estrutura - Configuração do ESLint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import {Heading, Text} from '@ignite-ui/react';
+import { Heading, Text } from '@ignite-ui/react'
 
 export default function Home() {
   return (
     <>
-     <Heading as='h1'>Home</Heading>
-     <Text>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut asperiores, debitis laborum ipsum dolorum beatae exercitationem minus aliquam laboriosam, doloribus, adipisci autem sint! Fuga officia ducimus commodi laborum autem. Molestias?</Text>
+      <Heading as="h1">Home</Heading>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut asperiores,
+        debitis laborum ipsum dolorum beatae exercitationem minus aliquam
+        laboriosam, doloribus, adipisci autem sint! Fuga officia ducimus commodi
+        laborum autem. Molestias?
+      </Text>
     </>
   )
 }

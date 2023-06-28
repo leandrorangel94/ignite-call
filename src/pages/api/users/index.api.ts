773882ee04aba4b2f49d8969667c23062c110d95
Leandro Rangel <rangel94@outlook.com>
2023-06-28 15:19:12 -0300

    Cadastro de usuário - Persistindo usuário no Prisma

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { prisma } from '@/src/lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') return res.status(405).end()
+
+  const { name, username } = req.body
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  res.status(201).json(user)
+}

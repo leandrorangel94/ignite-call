02a5dd5f963f064f4cd63f1e75dc973b3c1aae20
Leandro Rangel <rangel94@outlook.com>
2023-07-10 17:21:38 -0300

    Calendário e disponibilidade - Página de confirmação

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,42 @@
+import { Button, Text, TextInput, TextArea } from '@ignite-ui/react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+import { CalendarBlank, Clock } from 'phosphor-react'
+
+export function ConfirmStep() {
+  return (
+    <ConfirmForm>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm"></Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
